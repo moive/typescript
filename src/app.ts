28394eb0ca064aa028b43acb023c2bf66b8769b6
Moises Velasquez <mvelasquez@pictoric.com>
2019-08-13 17:05:04 -0500

    Implement error

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,34 @@
-try {
-    let name : string = 'Moises';
-    name = name.toUpperCase();
-    throw new Error("You shouldn't capitalize a name.");
-} catch (e) {
-    let err = <Error>e;
-    console.log('name: ', err.name);
-    console.log('message: ', err.message);
-    console.log('stack: ', err.stack);
-    
-    
+class DivideZeroException implements Error {
+    constructor(public name: string, public message: string, public numerator: number){
+        //
+    }
+
 }
+
+
+function dividir(numerator: number, denominator: number): number | null {
+    try{
+        if(denominator === 0){
+            throw new DivideZeroException("DivideByZero",
+            "You can't divide by zero", numerator);
+        }
+
+        return numerator/denominator;
+    } catch(e){
+        if(e instanceof DivideZeroException){
+            console.log('name: ', e.name);
+            console.log('message', e.message);
+            console.log('numerator: ', e.numerator);
+        }
+        else{
+            let err = <Error>e;
+            console.log(err.message);
+            
+        }
+        return null;
+    }
+}
+
+console.log(dividir(2,3));
+console.log(dividir(2,0));
+
