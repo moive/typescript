d6f18bd757bcfa1167c571f5734459cf215945c2
Moises Velasquez <mvelasquez@pictoric.com>
2019-08-13 18:14:43 -0500

    interface explicit

--- src/app.ts
+++ src/app.ts
@@ -1,55 +1,21 @@
-abstract class Animal {
-    public  annoy() {
-        console.log("I annoy");
-        return this;
-    }
-
-    public abstract makeNoise(): void;
-}
-// Cannot create an instance of an abstract class
-// let animal = new Animal();
-
-class Dog extends Animal{
-    public makeNoise(): Dog {
-        console.log("auf auf");
-        return this;
-    }
-
-    public bites(): Dog{
-        console.log('Bites his victim');
-        return this;
-    }
-
-    public runAway(): Dog{
-        console.log('runs away with his tail between legs.');
-        return this;
-        
-    }
-
-    public playDead(): void{
-        console.log("...");   
-    }
+interface EmailMessage {
+    body: string,
+    subject: string,
+    email: string,
+    priority: boolean
 }
 
-class Cat extends Animal{
-    public makeNoise(): void{
-        console.log("miau");
-    }
 
-    public beIndifferent(): void{
-        console.log("I don't care about anything");
-        
-    }
+function sendMessage(message: EmailMessage){
+    // send message
+    console.log(message)
 }
 
-function startMakingNoise(animal: Animal): void{
-    animal.makeNoise();
-}
+let message: EmailMessage = {
+    body: "This is body",
+    subject: "This is subject",
+    email: "example@gmail.com",
+    priority: true
+};
 
-let dog = new Dog();
-let cat = new Cat();
-
-dog.makeNoise().bites().bites().annoy().bites().playDead();
-
-
-
+sendMessage(message);
