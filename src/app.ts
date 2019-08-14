b2b890d01a137f45dfb64d22dad1d558dd52e1fb
Moises Velasquez <mvelasquez@pictoric.com>
2019-08-14 17:46:45 -0500

    generic

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,6 @@
-interface Transformer {
-    transformerString(value: string): string;
+function generic<T>(value: T): T{
+    return value;
 }
 
-class Uppercase implements Transformer{
-    public transformerString(value: string): string{
-        return value.toUpperCase();
-    }
-}
-
-class Reversed implements Transformer {
-    transformerString(value: string): string{
-        let valueReversed: string = value.split('')
-        .map((char, index, arr) => arr[arr.length - 1 - index]).join('');
-
-        return valueReversed;
-    }
-}
-
-function processing(value: string, ...transformations: Transformer[]): string{
-    let answer: string = value;
-    for (let i = 0; i < transformations.length; i++) {
-        let transformation = transformations[i];
-        answer = transformation.transformerString(answer);
-        
-    }
-    return answer;
-}
-
-let uppercase = new Uppercase();
-let reversed = new Reversed();
-
-let namePerson = "Moises";
-let namePerson2 = "Veronika";
-let namePerson3 = "Daniela";
-
-console.log(processing(namePerson, uppercase));
-console.log(processing(namePerson2, reversed));
-console.log(processing(namePerson3, uppercase, reversed));
+generic<string>("Moises") // Explicit
+generic(true) // Implicit
