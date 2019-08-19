94c5ca7e6f39eccbb9bd78cc152a88bfb51fbe9c
Moises Velasquez <mvelasquez@pictoric.com>
2019-08-19 14:10:36 -0500

    more examples extending generic

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,24 @@ interface AnyKindOfObject {
     what: string;
 }
 
-interface reuseableTypes<T extends object>{
+interface reusableTypes<T extends object>{
     entity: T;
 }
 
-const d: reuseableTypes<AnyKindOfObject> = {entity: {what: "wert"}}
+const d: reusableTypes<AnyKindOfObject> = {entity: {what: "wert"}}
 console.log(d.entity.what)
+
+
+interface ObjectWithId{
+    id: number;
+    what: string;
+}
+
+interface ReusableInterface<T extends {id: number}>{
+    entity: T;
+}
+
+// const e: ReusableInterface<AnyKindOfObject> = {entity: {id: 5, what: 1}};
+
+const f: ReusableInterface<ObjectWithId> = { entity: {id:1, what: "hi"}};
+console.log(f.entity.what);
