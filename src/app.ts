3b354a26416de4f0dccde1462b46518eb6717cb1
Moises Velasquez <mvelasquez@pictoric.com>
2019-08-19 15:06:51 -0500

    intersection generic

--- src/app.ts
+++ src/app.ts
@@ -24,3 +24,27 @@ interface ReusableInterface<T extends {id: number}>{
 
 const f: ReusableInterface<ObjectWithId> = { entity: {id:1, what: "hi"}};
 console.log(f.entity.what);
+
+
+
+interface WithId {
+    id:number;
+}
+
+interface User{
+    name: string;
+}
+
+interface Developer extends User{
+    favoriteLanguage: string;
+}
+
+function identifyUser<T extends User>(user: T): T & WithId {
+    const newUser = (<any>Object).assign({}, user, {id: 1});
+    return newUser;
+}
+
+const user: Developer = {name: "Patrick", favoriteLanguage: "Typescript"};
+const userWithId = identifyUser(user);
+console.log(userWithId)
+console.log(`${userWithId.name} (${userWithId.id}) favorite language is ${userWithId.favoriteLanguage}`);
