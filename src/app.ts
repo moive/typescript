39f491b8382dee04cd4485a0d108df621edec14d
Moises Velasquez <mvelasquez@pictoric.com>
2019-08-20 14:11:36 -0500

    decorator

--- src/app.ts
+++ src/app.ts
@@ -1,94 +1,19 @@
-//Mapped Partial
-
-interface Todo{
-    title: string;
-    description: string;
+//Decorators
+function ClassConsoleLog(target: any){
+    console.log("class", target);
+    target.prototype.created = new Date();
 }
 
-function updateTodo(todo: Todo, fieldsToUpdate: Partial<Todo>){
-    return {...todo, ...fieldsToUpdate}
+function ClassWas(target: any){
+    console.log("instantiated");
 }
 
-const partial1 = {
-    title: 'organize desk',
-    description: 'clear cluster'
+@ClassConsoleLog
+@ClassWas
+class Person {
+    created?: Date;
 }
 
-const partial2 = updateTodo(partial1, {
-    description: 'throw out trash'
-});
-
-console.log(partial2)
-
-
-//Mapped Readonly
-interface TodoReadonly{
-    title: string;
-}
-const readonly1 : Readonly<Todo> = {
-    title: 'Delete inactive user',
-    description: ''
-}
-//Cannot assign to 'title' because it is a read-only property.
-//readonly1.title = 'Hello';
-
-
-//Mapped Record<K,T>
-
-interface PageInfo{
-    title: string;
-}
-
-type Page = 'home' | 'about' | 'contact';
-
-const record1: Record<Page, PageInfo> = {
-    about: {title: 'about'},
-    contact: {title: 'contact'},
-    home: {title: 'home'}
-};
-
-console.log(record1);
-
-//Mapped Pick<T,K>
-
-interface TodoPick extends Todo{
-    completed: boolean;
-}
-
-type TodoPreview = Pick<TodoPick, 'title' | 'completed'>;
-
-const pick1: TodoPreview = {
-    title: 'Clean room',
-    completed: false
-};
-
-console.log(pick1);
-
-//Mapped Omit<T,K>
-
-interface TodoOmit extends Todo{
-    completed: boolean;
-}
-
-type TodoPreviewOmit = Omit<TodoOmit, 'description'>;
-
-const omit1: TodoPreviewOmit = {
-    title: 'Clean room',
-    completed: true
-}
-
-console.log('omit', omit1)
-
-//Mapped Required<T>
-
-interface Props{
-    title: string,
-    description?: string
-}
-
-const required1: Props = {title: 'hello'};
-
-const required2: Required<Props> = {title: 'hi', description:'lorem ipsum'}
-
-console.log('required1', required1)
-console.log('required2', required2)
+let p = new Person();
+let d = p.created;
+console.log(p.created);
