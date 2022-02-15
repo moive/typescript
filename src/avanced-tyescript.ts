057a21de417dcd8952b67e3850012aa28b32fbd8
Moises Velasquez <mvelasquez@pictoric.com>
2022-02-14 21:54:14 -0500

    working with Readonly

--- src/avanced-tyescript.ts
+++ src/avanced-tyescript.ts
@@ -6,3 +6,15 @@ type Status = "sad" | "happy";
 type Palette<T extends Status> = T extends "sad" ? DarkColors : LightColors;
 
 export const palette: Palette<"happy"> = "white";
+
+const sampleArray = [1, 2, 3];
+const otherArray = [2, 4, 6, 8];
+
+const tailMutable = <T>(array: T[]): T[] => (array.shift(), array);
+const tailInmutable = <T>(array: Readonly<T[]>): T[] => {
+	const [, ...tail] = array || [];
+	return tail;
+};
+
+console.log(sampleArray, tailMutable(sampleArray));
+console.log(otherArray, tailInmutable(otherArray));
