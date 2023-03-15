2f7a1134275b22376815d9167d42141ce483016b
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-03-15 17:49:50 -0500

    extends class

--- src/ts-herrera/scripts/06-class-hero.ts
+++ src/ts-herrera/scripts/06-class-hero.ts
@@ -1,13 +1,15 @@
-class Hero {
-	// alterEgo: string;
-	// age:number;
-	// realName:string;
+class Persona {
+	constructor(public name: string, public address: string) {}
+}
 
+class Hero extends Persona {
 	constructor(
 		public alterEgo: string,
 		public age: number,
 		public realName: string
-	) {}
+	) {
+		super(realName, "New York, USA");
+	}
 }
 
 const iroman = new Hero("Ironman", 45, "Tony");
